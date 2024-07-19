bceb553ad5b898f6c109550a957b168aa71ee082
John Doe <johndoe@example.com>
2024-07-20 02:41:36 +0300

    added columns, styled taskChips

--- client/src/components/task/TaskTable.js
+++ client/src/components/task/TaskTable.js
@@ -1,15 +1,28 @@
 import TaskChip from "./task-chip/TaskChip";
 import "./TaskTable.css";
 const TaskTable = (props) => {
-    return (
-        <div className="TaskTable">
-            <div className="task-table-inner">
-
-            </div>
-            <div className="task-table-inner">
-                {props.data.map(data => <TaskChip data={data}/>)}
-            </div>
+  return (
+    <div className="TaskTable">
+      <div className="task-table-inner">
+        <div className="justify-center d-flex">TODO</div>
+      </div>
+      <div className="task-table-inner">
+        <div className="justify-center d-flex">IN PROGRESS</div>
+        <div className="justify-center d-flex">
+          <div>
+            {props.data.map((data) => (
+              <TaskChip data={data} />
+            ))}
+          </div>
         </div>
-    )
-}
+      </div>
+      <div className="task-table-inner">
+        <div className="justify-center d-flex">REVIEW</div>
+      </div>
+      <div className="task-table-inner">
+        <div className="justify-center d-flex">DONE</div>
+      </div>
+    </div>
+  );
+};
 export default TaskTable;
